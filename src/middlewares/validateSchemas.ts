20221081ce117765d55bbe5a91cac52f79ec1ec7
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2023-12-26 20:08:06 -0300

    feat: :sparkles: add validate schema middleware

---
+++ src/middlewares/validateSchemas.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../interfaces/customError";
+import Joi from "joi";
+
+export default function validateSchemas(schema: Joi.ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      console.log('erro validação', error);
+      return res.status(422).send(error.details.map((detail: CustomError) => detail.message));
+    }
+    next();
+  }
+}
